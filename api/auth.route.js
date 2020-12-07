33cb24dd5888fd3a32ab4c5e7e8fef1697b181c9
hyzen0 <hail4heisenberg@gmail.com>
2020-12-07 15:33:29 +0530

    auth route.

--- api/auth.route.js
+++ api/auth.route.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 // Load Controllers
-const { registerController } = require("../controllers/auth.controller");
+const {
+  registerController,
+  activationController,
+} = require("../controllers/auth.controller");
 
 const { validSign } = require("../helpers/valid");
 
 router.post("/register", validSign, registerController);
+router.post("/activation", activationController);
 
 module.exports = router;
--- helpers/valid.js
+++ helpers/valid.js
@@ -17,31 +17,3 @@ exports.validSign = [
     .matches(/\d/)
     .withMessage("password must contain a number"),
 ];
-
-exports.validLogin = [
-  check("email").isEmail().withMessage("Must be a valid email address"),
-  check("password", "password is required").notEmpty(),
-  check("password")
-    .isLength({
-      min: 6,
-    })
-    .withMessage("Password must contain at least 6 characters")
-    .matches(/\d/)
-    .withMessage("password must contain a number"),
-];
-
-exports.forgotPasswordValidator = [
-  check("email")
-    .not()
-    .isEmpty()
-    .isEmail()
-    .withMessage("Must be a valid email address"),
-];
-
-exports.resetPasswordValidator = [
-  check("newPassword")
-    .not()
-    .isEmpty()
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least  6 characters long"),
-];
